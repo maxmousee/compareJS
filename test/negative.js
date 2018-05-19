2ce7bc17877effc56a2f65447b55de3378428e27
Natan Facchin <maxmousee@gmail.com>
2018-05-19 20:04:39 +0200

    Adding different data functional test case

---
+++ test/negative.js
@@ -0,0 +1,65 @@
+let mongoose = require("mongoose");
+let ComparableJSON = require('../models/comparablejson');
+let JSONDifferences = require('../models/jsondifferences');
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('ComparableJSON', () => {
+  beforeEach((done) => {
+    done();
+  });
+  
+  /*
+  * Test the /POST route, different data
+  */
+  describe('/POST left data', () => {
+    it('it should POST a valid base64 data to the left side', (done) => {
+      let data = {
+        data: "YWxnbG1hIGNvdXssphfIGFv"
+      }
+      chai.request(server)
+      .post('/v1/diff/6/left')
+      .send(data)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('id');
+        res.body.should.have.property('left');
+        done();
+      });
+    });
+  });
+ describe('/POST right data', () => {
+  it('it should POST a valid base64 data to the right side', (done) => {
+    let data = {
+      data: "YWxnbG1hIGNvdXsJJJJ12sphfIGFv"
+    }
+    chai.request(server)
+    .post('/v1/diff/6/right')
+    .send(data)
+    .end((err, res) => {
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      res.body.should.have.property('id');
+      res.body.should.have.property('right');
+      done();
+    });
+  });
+});
+  describe('/GET base64 different data', () => {
+    it('it should GET all the base64 data', (done) => {
+      chai.request(server)
+      .get('/v1/diff/6')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        done();
+      });
+    });
+  });
+});
